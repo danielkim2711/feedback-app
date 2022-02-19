01aceda9d951ab722f5e2287406b286734f30c7a
Daniel Kim <danielkim2711@gmail.com>
2022-02-19 16:46:54 +1300

    Define FeedbackItem component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from './components/Header';
+import FeedbackItem from './components/FeedbackItem';
 
 function App() {
   return (
     <>
       <Header />
-      <div className='container'></div>
+      <div className='container'>
+        <FeedbackItem />
+      </div>
     </>
   );
 }

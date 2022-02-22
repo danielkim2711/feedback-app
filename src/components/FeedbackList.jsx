7827e70773a75d07adce8d94bf5a30155dbfe8ef
Daniel Kim <danielkim2711@gmail.com>
2022-02-22 16:41:57 +1300

    Define Spinner component

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,44 +1,45 @@
 import { motion, AnimatePresence } from 'framer-motion';
 import { useContext } from 'react';
 import FeedbackItem from './FeedbackItem';
+import Spinner from './shared/Spinner';
 import FeedbackContext from '../context/FeedbackContext';
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  return (
-    <>
-      {!feedback || feedback.length === 0 ? (
-        <p>No Feedback Yet</p>
-      ) : (
-        <div className='feedback-list'>
-          <AnimatePresence>
-            {feedback.map((item) => (
-              <motion.div
-                key={item.id}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-              >
-                <FeedbackItem key={item.id} item={item} />
-              </motion.div>
-            ))}
-          </AnimatePresence>
-        </div>
+  if (!isLoading && (!feedback || feedback.length === 0)) {
+    return <p>No Feedback Yet</p>;
+  }
 
-        // No animation
-        // <div className='feedback-list'>
-        //   {feedback.map((item) => (
-        //     <FeedbackItem
-        //       key={item.id}
-        //       item={item}
-        //       handleDelete={handleDelete}
-        //     />
-        //   ))}
-        // </div>
-      )}
-    </>
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <div className='feedback-list'>
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem key={item.id} item={item} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </div>
   );
+
+  // No animation
+  // <div className='feedback-list'>
+  //   {feedback.map((item) => (
+  //     <FeedbackItem
+  //       key={item.id}
+  //       item={item}
+  //       handleDelete={handleDelete}
+  //     />
+  //   ))}
+  // </div>
 }
 
 export default FeedbackList;
